850997df88570ce73767b7e281adeeca838324c6
Anshika <anshikamittal535@gmail.com>
2026-07-17 23:11:28 +0530

    add_variables.py

---
+++ add _variables.py
@@ -0,0 +1,32 @@
+print("My name is Anshika", "my age is 17" )
+print(23)
+print(23+35)
+name ="Anshika"
+age = 17
+price = 25.99 
+age2 =age
+print(age)
+print(name)
+print("my name is :" ,name)
+print(age2)
+print(type(name))
+print(type(age))
+print(type(price))
+old =True
+print(type(old))
+# arithmatic opreator
+a =23
+b =78
+print(a*b)
+# relational operator
+a =50
+b =8
+print(a == b)
+print( a != b)
+#type conversion
+a =int("2") 
+b = 4.35
+sum = a + b 
+print (a +  b)
+name = input("enter your name:")
+print("welcome",name )
