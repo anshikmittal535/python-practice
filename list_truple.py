340d0cd5d30e1568b0063b51997cdb7f8e7a54b5
Anshika <anshikamittal535@gmail.com>
2026-07-19 14:01:51 +0530

    list_truple.py

---
+++ list_truple.py
@@ -0,0 +1,31 @@
+str ="This is a string.\n we are creating in a python ."
+print(str)
+str1 ="Anshika"
+len1 =len(str1)
+print(len1)
+str2 ="Mittal"
+final_str = str1+ " " + str2
+print(final_str) 
+str = "apna collage"
+ch = str[0]
+print(ch)
+str = "apna collage"
+print(str[1:4])
+print (str[ :5])
+print (str[0: ])
+str = "Apple"
+print(str[-3:-1])
+str = " I am studying python for apna collage "
+print (str.endswith("ege"))
+print(str.capitalize()) 
+print(str)
+print(str.replace("o" ,"a"))
+print(str.replace("python" , "javascript"))
+print(str.find("o"))
+print(str.count("m"))
+name = (input("Anshika")) 
+print(len (name))
+age = 21
+if (age>=18):
+    print("can vote and apply for license ")
+
