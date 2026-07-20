0e6b1e6a5928ad8251f80a637fd1c894d6834b16
Anshika <anshikamittal535@gmail.com>
2026-07-20 21:05:16 +0530

    if_else.py

---
+++ if_else.py
@@ -0,0 +1,57 @@
+age =21 
+if (age >= 18 ):
+    print("can vote and apply for license")
+    print("can vote")
+    print("can drive")
+light = "pink" 
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("light is broken")
+
+num = 5 
+if(num>2) :
+    print( "greater than 2 ") 
+elif(num>3):
+    print("greater than 3") 
+age = 14 
+if (age >= 18):
+    print("can vote")
+else: 
+    print("can not vote") 
+
+    marks = int(input("enter student marks :")) 
+if (marks >= 90):
+    grade = "A"
+elif(marks >=80 and marks < 90):
+    grade = "B"
+elif(marks >= 70 and marks < 80):
+    grade = " c "
+else:
+    grade = " D " 
+    
+print("grade of the student ->" , grade)
+
+# nesting
+age = 34 
+if (age >= 18):
+    if (age >= 80):
+        print ("can not drive ")
+    else:
+        print ("can drive")
+else:
+    print("can not drive")
+    
+    
+
+
+
+
+
+
+
+   
