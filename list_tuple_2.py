1e3b6d8394fe2769bfa69591bcf5e50f428102ea
Anshika <anshikamittal535@gmail.com>
2026-07-22 00:14:54 +0530

    list_tuple_2.py

---
+++ list_tuple_2.py
@@ -0,0 +1,70 @@
+marks1 = 94.4 
+marks2 = 87.4
+marks3 = 95.5
+marks4 = 66.3
+marks5 = 45.6
+marks = [94.4, 87.4, 95.5,66.3, 45.6] 
+print (marks)
+print(type(marks))
+print(marks[0])
+print(marks[3])
+print(len(marks))
+student = ["anshika", 98,"delhi", 14]
+print (student)
+print(student[0])
+student[0] = "arjun"
+print(student)
+marks = [23,45,67,89,40]
+print(marks[1:])
+print(marks[-3:-1]) 
+list =[2,1,3]
+list.append(4)
+print(list)
+list.sort()
+print(list)
+list.sort( reverse = True )
+print(list)
+list.reverse()
+print(list)
+list = [2,1,3]
+list.insert(1,5)
+print(list)
+list.pop(1)
+print(list)
+list = [2,1,1,3]
+list.remove(1)
+print(list)
+tup = (23,45,67,89,34 ,23)
+print(tup)
+print(type(tup))
+print (tup[1:3])
+print(tup.index (67))
+print(tup.count(23))
+movie1 ="3 idiots"
+movie2 = "drishyam"
+movie3 = "dangal"
+movie = ["3 idiots", "drishyam" , "dangal"]
+print(movie)
+list1 = [1,8,9,8,1]
+list2 = [1,2,3,4]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("palindrome")
+else:
+    print("not palindrome")
+
+
+tup = ["C", "D", "A", "A", "B", "B", "A"]
+print(tup.count("A"))
+tup.sort()
+print(tup)
+
+
+
+          
+
+
+
+
